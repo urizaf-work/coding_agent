681b686ff876ab0e5bc4b5a1c2782050ad8d2c6f
urizaf-work <uri.zafrir@kaltura.com>
2025-05-20 22:29:03 +0300

    change to claude-3-7-sonnet

--- run.rb
+++ run.rb
@@ -9,8 +9,14 @@ end
 require "ruby_llm"
 require_relative "src/agent"
 
+# RubyLLM.configure do |config|
+#   config.anthropic_api_key = ENV.fetch("ANTHROPIC_API_KEY", nil)
+#   config.default_model = "claude-3-7-sonnet"
+# end
+
 RubyLLM.configure do |config|
-  config.anthropic_api_key = ENV.fetch("ANTHROPIC_API_KEY", nil)
+  config.anthropic_api_key = ENV.fetch('ANTHROPIC_API_KEY', nil) # Or set your key directly
+  config.openai_api_base = "http://localhost:4000"      # Replace with your custom endpoint and port
   config.default_model = "claude-3-7-sonnet"
 end
 
